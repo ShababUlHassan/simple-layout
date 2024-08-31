85fd0e95af88d56def43954764d0b135887129a0
shabab <shababulhassan910@gmail.com>
2024-08-30 23:42:48 -0700

    added NEw file and edit Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useStudent } from '../Hooks/useContext'
+import React from "react";
+import { useStudent } from "../Hooks/useContext";
 
 const Footer = () => {
-    const {myName} = useStudent()
+  const { myName } = useStudent();
   return (
-    <div className='bg-gray-500'><p className='text-white text-center'>@CopyRight made by {myName}</p></div>
-  )
-}
+    <div className="bg-gray-500">
+      <p className="text-white text-center">@CopyRight 2024 made by {myName}</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
